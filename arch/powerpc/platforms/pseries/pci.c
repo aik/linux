325f100c1eda7f90fca46768cb18f53754746e66
Alexey Kardashevskiy <aik@ozlabs.ru>
2018-09-26 12:21:44 +1000

    powerpc/pseries/npu: Enable platform support
    We already changed NPU API for GPUs to not to call OPAL and the remaining
    bit is initializing NPU structures.
    
    This uses a new QEMU capability which marks NPU-enabled vPHBs as
    "IBM,npu-vphb" and initializes an NPU structure per vPHB.
    
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- arch/powerpc/platforms/pseries/pci.c
+++ arch/powerpc/platforms/pseries/pci.c
@@ -29,6 +29,7 @@
 #include <asm/pci-bridge.h>
 #include <asm/prom.h>
 #include <asm/ppc-pci.h>
+#include <asm/pci.h>
 #include "pseries.h"
 
 #if 0
@@ -237,6 +238,8 @@ static void __init pSeries_request_regions(void)
 
 void __init pSeries_final_fixup(void)
 {
+	struct pci_controller *hose;
+
 	pSeries_request_regions();
 
 	eeh_probe_devices();
@@ -246,6 +249,9 @@ void __init pSeries_final_fixup(void)
 	ppc_md.pcibios_sriov_enable = pseries_pcibios_sriov_enable;
 	ppc_md.pcibios_sriov_disable = pseries_pcibios_sriov_disable;
 #endif
+	list_for_each_entry(hose, &hose_list, list_node)
+		if (of_device_is_compatible(hose->dn, "IBM,npu-vphb"))
+			pnv_npu2_init(hose);
 }
 
 /*
