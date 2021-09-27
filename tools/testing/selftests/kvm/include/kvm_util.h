f8c77e6939d8ffbdabce83cd2b08f6a055bcf301
Fabiano Rosas <farosas@linux.ibm.com>
2021-09-27 16:08:27 -0400

    KVM: selftests: Add support for ppc64le
    This adds the infrastructure for writing tests for the powerpc
    platform (Only Radix MMU for now).
    
    This patch also enables two tests:
    
    - a dummy sample test that creates a guest with one vcpu, issues
      hypercalls and reads/writes test values from memory.
    
    - the kvm_page_table test, although at this point I'm not using it to
      test KVM, but mostly as a way to stress test this code.
    
    $ make ARCH=powerpc -C tools/testing/selftests/kvm/
    $ ./tools/testing/selftests/kvm/powerpc/sample_test
    $ ./tools/testing/selftests/kvm/kvm_page_table_test
    
    Signed-off-by: Fabiano Rosas <farosas@linux.ibm.com>

--- tools/testing/selftests/kvm/include/kvm_util.h
+++ tools/testing/selftests/kvm/include/kvm_util.h
@@ -48,6 +48,7 @@ enum vm_guest_mode {
 	VM_MODE_PXXV48_4K,	/* For 48bits VA but ANY bits PA */
 	VM_MODE_P47V64_4K,
 	VM_MODE_P44V64_4K,
+	VM_MODE_P51V52_64K,
 	NUM_VM_MODES,
 };
 
@@ -68,6 +69,12 @@ enum vm_guest_mode {
 #define VM_MODE_DEFAULT			VM_MODE_P44V64_4K
 #define MIN_PAGE_SHIFT			12U
 #define ptes_per_page(page_size)	((page_size) / 16)
+
+#elif defined(__powerpc__)
+
+#define VM_MODE_DEFAULT			VM_MODE_P51V52_64K
+#define MIN_PAGE_SHIFT			16U
+#define ptes_per_page(page_size)	((page_size) / 8)
 
 #endif
 
