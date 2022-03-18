3edf59a06cb3e50d68e0c173fccc2ca7dc53cc07
Sathvika Vasireddy <sv@linux.ibm.com>
2022-03-18 16:21:39 +0530

    objtool: Enable and implement 'mcount' subcommand
    This patch adds 'mcount' as a subcommand to objtool, and enables
    the same for x86. objtool is built if CONFIG_FTRACE_MCOUNT_USE_OBJTOOL
    is selected. Additionally, architectures can select HAVE_NOP_MCOUNT
    if they choose to nop out mcount call sites. If that config option is
    selected, then --mnop is passed as an option to 'objtool mcount'
    
    Signed-off-by: Sathvika Vasireddy <sv@linux.ibm.com>
    Signed-off-by: Alexey Kardashevskiy <aik@ozlabs.ru>

--- tools/objtool/include/objtool/builtin.h
+++ tools/objtool/include/objtool/builtin.h
@@ -10,11 +10,13 @@
 extern const struct option check_options[];
 extern bool no_fp, no_unreachable, retpoline, module, backtrace, uaccess, stats,
 	    lto, vmlinux, mcount, noinstr, backup, sls, dryrun,
-	    ibt;
+	    ibt, mnop;
 
 extern int cmd_parse_options(int argc, const char **argv, const char * const usage[]);
+extern int cmd_parse_options_mcount(int argc, const char **argv, const char * const usage[]);
 
 extern int cmd_check(int argc, const char **argv);
 extern int cmd_orc(int argc, const char **argv);
+extern int cmd_mcount(int argc, const char **argv);
 
 #endif /* _BUILTIN_H */
